0833ae67f6dd9693e716c337b7e764dfa841b8ed
David Siaw (via Travis CI) <david.siaw@mobingi.com>
2015-07-07 17:00:41 +0900

    db

--- fuel/app/config/production/db.php
+++ fuel/app/config/production/db.php
@@ -5,13 +5,13 @@
 
 return array(
 	'default' => array(
-		'type' => 'mysqli',
-		'connection'  => array(
+		'type' => "mysqli",
+		'connection' => array(
 			'hostname'   => getenv("MYSQLPROXY_PORT_3306_TCP_ADDR"),
 			'port'       => getenv("MYSQLPROXY_PORT_3306_TCP_PORT"),
 			'database'   => getenv("MOCLOUD_DATABASE_NAME"),
 			'username'   => getenv("MOCLOUD_DATABASE_USERNAME"),
-			'password'   => getenv("MOCLOUD_DATABASE_PASSWORD"),
+			'password'   => getenv("MOCLOUD_DATABASE_PASSWORD")
 		),
 	),
 );
